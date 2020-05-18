81a00d9e1d04ececabb34afaa11ab347d04083bc
Allyson de Oliveira Brandão <62918698+Allysonubius@users.noreply.github.com>
2020-05-18 20:25:04 -0300

    Iniciado aulas.

---
+++ src/main/java/com/example/CollectionsStreams/CollectionsStreamsApplication.java
@@ -0,0 +1,13 @@
+package com.example.CollectionsStreams;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollectionsStreamsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CollectionsStreamsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/CollectionsStreams/CollectionsStreamsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CollectionsStreams;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CollectionsStreamsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
